7316d7dfb45b56f12806e5cd59aa7dcca3574d75
Student <Student@FJPC-18>
2016-11-28 11:19:50 +0200

    Add project files.

---
+++ BooksBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BooksBlog.MvcApplication" Language="C#" %>
---
+++ BooksBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BooksBlog.Startup))]
+namespace BooksBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
